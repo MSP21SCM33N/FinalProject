dcdaeedbbed01caaedff7693bf4cc5aafb05a8a3
ajhosek <32681415+ajhosek@users.noreply.github.com>
2017-10-26 12:42:10 -0500

    Create Mazes.py

---
+++ Mazes.py
@@ -0,0 +1,15 @@
+#Maze 1
+x = 6
+y = 1
+maze = [
+        [b,b,b,b,b,b,b,b],
+        [b,n,n,n,n,n,n,b],
+        [b,n,b,b,n,b,b,b],
+        [b,n,n,n,n,b,g,b],
+        [b,b,n,b,b,b,n,b],
+        [b,n,n,b,n,n,n,b],
+        [b,n,n,n,n,b,n,b],
+        [b,b,b,b,b,b,b,b]
+    ]
+   
+#Maze 2
