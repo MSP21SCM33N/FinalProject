05fac1953f7fc1132948968a53f823522f1dc9d5
ajhosek <32681415+ajhosek@users.noreply.github.com>
2017-10-31 11:49:30 -0500

    Update Mazes.py

--- Mazes.py
+++ Mazes.py
@@ -13,3 +13,29 @@ maze = [
     ]
    
 #Maze 2
+x = 6
+y = 2
+maze = [
+        [b,b,b,b,b,b,b,b],
+        [b,n,n,n,n,n,n,b],
+        [b,b,n,b,b,b,n,b],
+        [b,n,n,b,n,b,b,b],
+        [b,n,n,n,n,b,g,b],
+        [b,n,b,b,n,b,n,b],
+        [b,n,n,b,n,n,n,b],
+        [b,b,b,b,b,b,b,b]
+        ]
+#Maze 3
+x = 3
+y = 6
+maze = [
+        [b,b,b,b,b,b,b,b],
+        [b,n,n,n,b,n,n,b],
+        [b,n,b,b,b,b,n,b],
+        [b,n,b,n,n,n,n,b],
+        [b,n,n,n,b,b,n,b],
+        [b,n,b,b,b,g,n,b],
+        [b,n,n,n,b,n,n,b],
+        [b,b,b,b,b,b,b,b]
+        ]
+         
